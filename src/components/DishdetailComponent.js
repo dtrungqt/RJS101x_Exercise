e98e1c373fafe6beded1935289abc9d4c9c58e31
DTrung <ngdinhtrung.qt@gmail.com>
2022-07-08 19:25:30 +0700

    3 render methods

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -17,38 +17,55 @@ class DishDetail extends Component {
     super(props);
   }
 
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
+  renderComments(comments) {
+    if (comments != null) {
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <ul className="list-unstyled">
+            {comments.map((comment) => {
+              return (
+                <li key={comment.id}>
+                  <p>{comment.comment}</p>
+                  <p>
+                    --{comment.author}, {comment.date}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
   //method hiển thị thông tin món ăn và các bình luận của món ăn đã được chọn
   render() {
     const dish = this.props.selectedDish;
-
     if (dish != null) {
       return (
         <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            <Card>
-              <CardImg width="100%" src={dish.image} alt={dish.name} />
-              <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-              </CardBody>
-            </Card>
-          </div>
-
-          <div className="col-12 col-md-5 m-1">
-            <h4>Comments</h4>
-            <ul className="list-unstyled">
-              {dish.comments.map((comment) => {
-                return (
-                  <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>
-                      --{comment.author}, {comment.date}
-                    </p>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+          {this.renderDish(dish)}
+          {this.renderComments(dish.comments)}
         </div>
       );
     } else {
